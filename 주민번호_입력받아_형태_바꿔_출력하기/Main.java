ed45f2b677791ddd267a77346a6a902b35a957cc
suji5262 <suji5262@gmail.com>
2023-06-21 20:29:41 +0900

    :sparkles: 주민번호_형태바꿔_출력 file add

---
+++ 주민번호_입력받아_형태_바꿔_출력하기/Main.java
@@ -0,0 +1,15 @@
+package 주민번호_입력받아_형태_바꿔_출력하기;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        input = input.replaceAll("-", "");
+
+        System.out.println(input);
+    }
+}
