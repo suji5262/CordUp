96444ff36f35e40a9187c111f3c54023ebec51b7
suji5262 <suji5262@gmail.com>
2023-06-23 16:06:12 +0900

    :sparkles: 문장_1개_입력받아_그대로_출력하기 file add

---
+++ Ex_100/문장_1개_입력받아_그대로_출력하기/Main.java
@@ -0,0 +1,13 @@
+package Ex_100.문장_1개_입력받아_그대로_출력하기;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str);
+    }
+}
