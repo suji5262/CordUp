cd9cd0ef8d389b0ed2e4947da50a7c1e54088e69
suji5262 <suji5262@gmail.com>
2023-06-23 16:05:58 +0900

    :sparkles: 단어_1개_입력받아_그대로_출력하기 file add

---
+++ Ex_100/단어_1개_입력받아_그대로_출력하기/Main.java
@@ -0,0 +1,14 @@
+package Ex_100.단어_1개_입력받아_그대로_출력하기;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(str);
+
+    }
+}
